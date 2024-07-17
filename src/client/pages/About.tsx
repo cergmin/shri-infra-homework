a724b5b8aafe4fb794740a1314de02648e85b80a
pltnkvart <artosi@yandex.ru>
2024-07-17 13:43:38 +0300

    change name and fix test

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -5,17 +5,15 @@ import { cn } from "@bem-react/classname";
 const bem = cn("About");
 
 export const About: React.FC = () => {
-  return (
-    <div className={bem()}>
-      <Helmet title="About" />
-      <div className="row">
-        <div className="col">
-          <h1>About</h1>
-          <p>
-            Author: [Your Name]
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={bem()}>
+			<Helmet title="About" />
+			<div className="row">
+				<div className="col">
+					<h1>About</h1>
+					<p>Author: Плотников Артем</p>
+				</div>
+			</div>
+		</div>
+	);
 };
