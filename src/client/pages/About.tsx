86f27728fb0f746eb30c50eb779ad61d5420a38a
nchertolin <nchertolin@gmail.com>
2024-07-17 14:09:00 +0500

    fix: change author name in about page and in about test

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -5,17 +5,17 @@ import { cn } from "@bem-react/classname";
 const bem = cn("About");
 
 export const About: React.FC = () => {
-  return (
-    <div className={bem()}>
-      <Helmet title="About" />
-      <div className="row">
-        <div className="col">
-          <h1>About</h1>
-          <p>
-            Author: [Your Name]
-          </p>
+    return (
+        <div className={bem()}>
+            <Helmet title="About" />
+            <div className="row">
+                <div className="col">
+                    <h1>About</h1>
+                    <p>
+                        Author: Chertolin Nikita
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
