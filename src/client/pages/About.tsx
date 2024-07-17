757dfb3958d84e64447ff1c58c0001ed362f1170
Sergey Otrubyannikov <s.otrubyannikov@gmail.com>
2024-07-17 11:58:51 +0300

    fix about.test

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,9 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>
-            Author: [Your Name]
-          </p>
+          <p>Author: Sergey</p>
         </div>
       </div>
     </div>
