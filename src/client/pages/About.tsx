7d51c61f03a5ef34e6c7e466bd7fe53f2b35ddb6
Ярослав Патрикеев <yaroslav@MacBook-Pro--Yaroslav.local>
2024-07-17 16:07:24 +0300

    поменял автора на странице About

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: [Your Name]
+            Author: Yaroslav Patrikeev
           </p>
         </div>
       </div>
