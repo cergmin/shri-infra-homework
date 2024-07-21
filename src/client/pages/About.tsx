55b664a757a0448a0b5afcf97df1afa435e1e2c2
OsipovaES <lizinbonik474@gmail.com>
2024-07-21 22:21:13 +0300

    fix name

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: [Your Name]
+            Author: Osipova Elizaveta
           </p>
         </div>
       </div>
