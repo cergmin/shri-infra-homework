d51469b69d85420a51452c7bea57343254282e95
Alexandr Borzov <borzov.02@mail.ru>
2024-07-21 22:12:10 +0300

    Rename link

--- src/client/Application.tsx
+++ src/client/Application.tsx
@@ -32,8 +32,8 @@ export const Application: FC = () => {
             <nav className='navbar navbar-expand-sm navbar-light bg-light'>
                 <div className='container'>
                     <Link className={bem('Brand', ['navbar-brand'])} to='/'>
-                        Kogtetochka store
-                    </Link>
+                        My Kogtetochka store
+                    </Linsdk>
                     <button className={bem('Toggler', ['navbar-toggler'])} aria-label='Toggle navigation' onClick={toggle}>
                         <span className='navbar-toggler-icon'></span>
                     </button>
