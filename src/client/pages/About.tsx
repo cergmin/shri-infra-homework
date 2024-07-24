dee6de28cb842138d5b938c9b2f6dc5806694aef
despores <ernest.sakhabutdinov@mail.ru>
2024-07-24 15:14:14 +0300

    Changed author name

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Osipova Elizaveta
+            Author: Ernest Sakhabutdinov
           </p>
         </div>
       </div>
