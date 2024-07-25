af5d8a78901ff0be4ee5ee7e614962d6881b7c12
polinabubb <polinabubb@gmail.com>
2024-07-25 13:47:21 +0500

    для пулл реквеста

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Nataly
+            Author : Nataly
           </p>
         </div>
       </div>
--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Nataly');
+        expect(container.textContent).toBe('AboutAuthor : Nataly');
     });
 });
